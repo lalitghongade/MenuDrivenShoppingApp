b44a27375fe512f8c218728409886f4ad730420c
lalit ghongade <lalitghongade1451999@gmail.com>
2021-08-18 15:57:19 +0530

    changes pushed

--- com.lala/src/main/java/com/lala/Menu.java
+++ com.lala/src/main/java/com/lala/Menu.java
@@ -14,13 +14,14 @@ public class Menu {
 	public void starMenu() {
 		Scanner scanner = new Scanner(System.in);
 		int choice=0;
-		
+		do {
 		log.info("-----------------------------");
 		log.info("Welecome to the Shopping App");
 		log.info("-----------------------------");
 		log.info("1. Login As Customer");
 		log.info("2. Login As Employee");
 		log.info("3. Register As Customer");
+		log.info("4. Exit");
 		log.info("-----------------------------");
 		log.info("-----------------------------");
 		log.info("Enter Your Choice");
@@ -35,24 +36,29 @@ public class Menu {
 		{
 			case 1:
 					//Login as customer case
-					log.info("still in development");
+					log.info("still in development case :"+1);
 				
 					break;
 			case 2:
 					//Login as employee case
-					log.info("still in development");
+					log.info("still in development case :"+2);
 					break;
 			case 3:
 					//register as customer case
-					log.info("still in development");
+					log.info("still in development case :"+3);
 					break;
+			case 4:
+					log.info("Thank You for your valuable Time ");
+					
+                    log.info("See you soon....Bye Bye!!!");
+                    break;
 			default:
 				//default case
 				log.info("invalid choice");
 				break;
 	}//switch block
 				
-		
+		}while(choice!=4);//do-while block
 		
 	}//startMenuMethod
 	
